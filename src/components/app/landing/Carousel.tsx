2a4d4a608f3e265c78f48883aea6a3c93581ca78
Daniel Toquica <j.toquica@udla.edu.co>
2024-09-08 21:33:03 -0500

    fix img-carrusell

--- src/components/app/landing/Carousel.tsx
+++ src/components/app/landing/Carousel.tsx
@@ -8,7 +8,6 @@ import {
 import Autoplay from 'embla-carousel-autoplay'
 import { useRef, useEffect } from 'react'
 import Fade from 'embla-carousel-fade'
-import { Card, CardContent } from '@/components/ui/card'
 
 const images = [
   { src: 'http://45.71.181.109/source-ua3d/imagen1.png', alt: 'Slide 1' },
@@ -16,7 +15,7 @@ const images = [
   { src: 'http://45.71.181.109/source-ua3d/imagen3.jpg', alt: 'Slide 3' }
 ]
 
-const CarouselComponent = () => {
+export default function CarouselComponent () {
   const autoplayPlugin = useRef(Autoplay({ delay: 10000, stopOnInteraction: true }))
   const fadePlugin = useRef(Fade())
 
@@ -36,35 +35,24 @@ const CarouselComponent = () => {
   }, [])
 
   return (
-    <div className='relative w-full h-screen overflow-hidden'>
       <Carousel
         plugins={[autoplayPlugin.current, fadePlugin.current]}
-        className='w-full h-full'
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        <CarouselContent className='w-full h-full'>
+        <CarouselContent>
           {images.map((image, index) => (
-            <CarouselItem key={index} className='w-full h-full flex-shrink-0'>
-              <div className='p-1'>
-                <Card>
-                  <CardContent className='flex aspect-square items-center justify-center p-6'>
-                    <img
-                      src={image.src}
-                      alt={image.alt}
-                      className='w-full h-full object-cover'
-                    />
-                  </CardContent>
-                </Card>
-              </div>
+            <CarouselItem key={index} className='flex-shrink-0'>
+                <img
+                  src={image.src}
+                  alt={image.alt}
+                  className='w-full h-full object-cover'
+                />
             </CarouselItem>
           ))}
         </CarouselContent>
         <CarouselPrevious className='absolute left-4 top-1/2 transform -translate-y-1/2 z-10 bg-white p-2 rounded-full shadow-lg cursor-pointer' />
         <CarouselNext className='absolute right-4 top-1/2 transform -translate-y-1/2 z-10 bg-white p-2 rounded-full shadow-lg cursor-pointer' />
       </Carousel>
-    </div>
   )
 }
-
-export default CarouselComponent
--- src/pages/Landing/HomePage.tsx
+++ src/pages/Landing/HomePage.tsx
@@ -3,12 +3,15 @@ import useDocumentTitle from '@/utils/useDocumentTitle'
 
 export default function HomePage () {
   useDocumentTitle('Inicio')
+
   return (
-    <div className='pt-20 relative w-full h-screen'>
-      <CarouselComponent />
-      <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white'>
-        <h1 className='text-4xl font-bold'>Bienvenidos a Uniamazonia 3D</h1>
+    <>
+      <div className='relative w-full h-screen pt-16'>
+        <CarouselComponent />
+        <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+          <h1 className='text-4xl font-bold text-white'>Bienvenidos a Uniamazonia 3D</h1>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
